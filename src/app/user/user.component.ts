b6bd76e8f8eecbf57b195d93a335c1a12ef81514
Sneza9 <spasic.sneska@gmail.com>
2024-10-25 23:38:40 +0200

    Update user and app component Add dummy data and pictures for users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-user';
+
+//floor 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -9,4 +13,18 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+  //Mozemo da dodamo property bez let var itd 
+  //Sve property-je koje dodajemo su dostupni u .html fajlu 
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath(){
+    return '../assets/users/' + this.selectedUser.avatar
+  }
+
+  //Metoda 
+  onSelectuser(){
+    console.log('Clicked!'); 
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser=DUMMY_USERS[randomIndex];
+  }
 }
